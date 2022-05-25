c61bda2961abf060a4ae10d378ebc6ccf11fc437
abdelquodr <salaudeenhyart@gmail.com>
2022-05-25 13:27:51 +0100

    working on userHeader

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -1,12 +1,47 @@
+import { useState } from 'react'
 import styles from './userheader.module.css'
 
 export default function UserHeader(){
+
+    const active = {
+      all: { isActive: true },
+      teams: { isActive: false },
+      personal: { isActive: false}
+    }
+
+    // state
+    const [ isActive, setIsActive ] = useState(active)
+
+    // handler
+    const handleClick = (e) => {
+      const name = e.target.innerText;
+        switch (name){
+          case "All":
+            setIsActive(prev => ({...prev, all: { isActive: true },teams: { isActive: false },
+              personal: { isActive: false} }));
+              break;
+          case 'Teams':
+            setIsActive(prev => ({ ...prev, teams: { isActive: true }, all: { isActive: false,
+              personal: { isActive: false} } }));
+              break;
+          case 'Personal':
+            setIsActive(prev => ({ ...prev, personal: { isActive: true}, all: { isActive: false }, teams: { isActive: false },
+               }));
+              break;
+           default:
+             return
+        }
+    }
+
+    const { all, teams, personal } = isActive;
+    // console.log(isActive)
+
     return (
      <div className={ styles.userHeader }>
         <ul className={ styles.userType }>
-          <li className={ styles.isActive }>All</li>
-          <li>Teams</li>
-          <li>Personal</li>
+          <li className={ all.isActive && styles.isActive } onClick={ handleClick } >All</li>
+          <li className={ teams.isActive && styles.isActive } onClick={ handleClick } >Teams</li>
+          <li className={ personal.isActive && styles.isActive } onClick={ handleClick } >Personal</li>
         </ul>
       </div>
     )
